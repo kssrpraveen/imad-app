a7149c27cd0e81e5713d64b84acc43a9f753bb37
kssrpraveen <praveen.kompella81@gmail.com>
2017-08-03 17:09:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articleone={
+
+var articles={
+articleone:{
     title:'article -one|praveen',
     heading:'hello',
     date:'sep 2 10',
     content:
-    `<p>hello this is praveen'</p>`
+    `<p>hello this is praveen .</p>`
+},
+articletwo:{
+    title:'article -two|praveen',
+    heading:'second',
+    date:'sep 15 2010',
+    content:
+    `<p>hello this is praveens second page</p>`
+},
+articlethree:{
+    title:'article -three|praveen',
+    heading:'third',
+    date:'sep 2 10',
+    content:
+    `<p>hello this is praveens third page'</p>`
+}
 };
 function createTemplate(data) {
     var title=data.title;
@@ -44,8 +61,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/articleone',function(req,res){
-res.send(createTemplate(articleone));
+app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {
