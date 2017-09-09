75fdcb6d91cf37c9f89657ce36a765de457b207a
kssrpraveen <praveen.kompella81@gmail.com>
2017-09-10 00:12:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,10 @@
-console.log('Loaded!');
-var element=document.getElementById('spa');
-element.innerHTML='10';
-var img=document.getElementById('madi');
-  var marginLeft=0;
-function moveRight()
-{
-  
-   marginLeft=marginLeft+10;
-   img.style.marginLeft=marginLeft+'px';
-    
-}
-img.onclick=function(){
-    
-    var interval=setInterval(moveRight,100);
-    
-};
-
 
 var counter=0;
 var button=getElementById('but');
 button.onclick=function(){
  counter=counter+1;
- var c=doucument.getElementById('spa');
- c.innerHTML='10';
+ var element=document.getElementById('spa');
+element.innerHTML='10';
 
  
 };
