34632c72114fbf6dfc83b51e6b87961084dee64e
kssrpraveen <praveen.kompella81@gmail.com>
2017-09-10 00:08:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,10 +15,13 @@ img.onclick=function(){
     var interval=setInterval(moveRight,100);
     
 };
+
+
+
 var counter=0;
 var button=getElementById('but');
 button.onclick=function(){
  counter=counter+1;
  var c=doucument.getElementById('spa');
- c.innerHTML=counter.toString();
+ c.innerHTML=10;
 };
