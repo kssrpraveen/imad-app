5837ffea6f8c8bc3a6cfec2e106204bc493d8237
kssrpraveen <praveen.kompella81@gmail.com>
2017-09-09 22:18:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML('new Value');
+var element=document.getElementById('main-text');
+element.innerHTML='New Value';
