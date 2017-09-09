c7b759bdd0d9e81a935688d5d075f66e1b2e4b4f
kssrpraveen <praveen.kompella81@gmail.com>
2017-09-09 22:50:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 var img=document.getElementById('madi');
-img.onClick=funrction(){
+img.onClick=function(){
     img.style.marginleft='100px';
     
 }
